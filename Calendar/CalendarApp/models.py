2ba5b7478d2c8ddabb1ba5d34e2c7b1f99cff9b6
Falkway <nowak1904@gmail.com>
2024-08-12 11:47:03 +0200

    widok kalendarza

--- Calendar/CalendarApp/models.py
+++ Calendar/CalendarApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class Event(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
--- Calendar/CalendarApp/views.py
+++ Calendar/CalendarApp/views.py
@@ -35,3 +35,35 @@ def logout_view(request):
     if request.method == 'POST':
         logout(request)
         return redirect('login')
+
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Event
+from .forms import EventForm
+
+@login_required
+def calendar_view(request):
+    events = Event.objects.filter(user=request.user)
+    form = EventForm()
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.user = request.user
+            event.save()
+            return redirect('calendar_view')
+    return render(request, 'calendar.html', {'events': events, 'form': form})
+
+@login_required
+def add_event(request):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            event = form.save(commit=False)
+            event.user = request.user
+            event.save()
+            return redirect('calendar_view')
+    else:
+        form = EventForm()
+    return render(request, 'add_event.html', {'form': form})
